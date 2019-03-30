e01d44224d87a3cc5f270127658a3a7a07fa5a07
zjunbin/GitTest <648260307@qq.com/zjunbin/GitTest>
2019-03-30 21:48:40 +0800

    导入根路径

--- case_run.py
+++ case_run.py
@@ -4,14 +4,14 @@
 # @Email     :648060307@qq.com
 # @File      :case_run.py
 import HTMLTestRunnerNew
+import sys
 import unittest
-
+# sys.path.append('./')
 from common import constants
 from cases import test_bidLoan,test_loadadd,test_withdraw,test_recharge,test_login,test_register
-
+# unittest.defaultTestLoader.discover(r'D:\api_testing\cases','test_*.py',top_level_dir=None)
 suite = unittest.TestSuite()
 loader = unittest.TestLoader()
-# suite.addTest(loader.loadTestsFromTestCase(Login))
 suite.addTest(loader.loadTestsFromModule(test_login))
 suite.addTest(loader.loadTestsFromModule(test_register))
 suite.addTest(loader.loadTestsFromModule(test_recharge))
