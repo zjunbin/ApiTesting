a1c7b217bcae176a80db76c5c4441dff376a6be2
zjunbin/GitTest <648260307@qq.com/zjunbin/GitTest>
2019-03-30 14:43:20 +0800

    测试2

--- case_run.py
+++ case_run.py
@@ -3,18 +3,22 @@
 # @Author    :zjunbin
 # @Email     :648060307@qq.com
 # @File      :case_run.py
-import HTMLTestRunnerEN
+import HTMLTestRunnerNew
 import unittest
 
 from api_testing.common import constants
-from api_testing.cases import test_login,test_register
+from api_testing.common import test_login,test_register,test_bidLoan,test_loadadd,test_recharge,test_withdraw
 
 suite = unittest.TestSuite()
 loader = unittest.TestLoader()
 # suite.addTest(loader.loadTestsFromTestCase(Login))
 suite.addTest(loader.loadTestsFromModule(test_login))
 suite.addTest(loader.loadTestsFromModule(test_register))
+suite.addTest(loader.loadTestsFromModule(test_recharge))
+suite.addTest(loader.loadTestsFromModule(test_withdraw))
+suite.addTest(loader.loadTestsFromModule(test_loadadd))
+suite.addTest(loader.loadTestsFromModule(test_bidLoan))
 with open(constants.result_path, 'wb+') as file:
-    run = HTMLTestRunnerEN.HTMLTestRunner(file)
+    run = HTMLTestRunnerNew.HTMLTestRunner(file)
     run.run(suite)
 
